eb68c4e4e2d908ff4115c795f4557806e3ee044e
CatherineAmir <catherine.amir20@gmail.com>
2025-07-16 16:59:32 +0300

    add time

--- static/src/js/timeoff_request.js
+++ static/src/js/timeoff_request.js
@@ -18,6 +18,8 @@ function on_change_leave_type(selectedLeaveType) {
     // It updates the leave type field in the form
     const selectedOption = selectedLeaveType.options[selectedLeaveType.selectedIndex];
     const attachmentField = selectedOption.dataset.document;
+    const request_unit = selectedOption.dataset.request_unit;
+
     const attachment_file = document.getElementById('document_attachment');
     const attachment_label = document.getElementById('attachment_label');
     if (attachmentField) {
@@ -30,6 +32,42 @@ function on_change_leave_type(selectedLeaveType) {
         attachment_file.required = false;
         attachment_label.classList.add('d-none');
     }
+    const half_day_div = document.getElementById('half_day_div');
+    const certain_time_div = document.getElementById('certain_time_div');
+    const custom_period_div = document.getElementById('custom_period_div');
+
+
+    console.log("Selected attachment field:", attachmentField);
+    console.log("Selected leave type:", request_unit);
+    if (request_unit === 'day') {
+        half_day_div.classList.add('d-none');
+        half_day_div.classList.remove('d-block');
+        certain_time_div.classList.add('d-none');
+        certain_time_div.classList.remove('d-block');
+        custom_period_div.classList.add('d-none');
+        custom_period_div.classList.remove('d-block');
+        date_to_div.classList.remove('d-none');
+        date_to_div.classList.add('d-block');
+
+    } else if (request_unit === 'half_day')
+        // half day check box will appear
+    {
+        half_day_div.classList.remove('d-none');
+        half_day_div.classList.add('d-block');
+        certain_time_div.classList.add('d-none');
+        certain_time_div.classList.remove('d-block');
+    } else if (request_unit === 'hour') {
+        certain_time_div.classList.add('d-block')
+        certain_time_div.classList.remove('d-none')
+        //     custom hour check box will appear
+        half_day_div.classList.add('d-block')
+        half_day_div.classList.remove('d-none')
+
+    } else {
+
+    }
+
+
 }
 
 function buttonCancel(event) {
@@ -62,7 +100,7 @@ function buttonDelete(id) {
     alertPopUp.classList.add('d-none');
     alertPopUp.classList.remove('d-block');
 
-    window.location.href = '/my/timeoffs/delete/'+id.toString();
+    window.location.href = '/my/timeoffs/delete/' + id.toString();
 
 
 }
@@ -78,8 +116,55 @@ function cancelDelete() {
     alertPopUp.classList.remove('d-block');
 
 
-
     // window.location.href = '/my/timeoffs';
 }
 
 
+function onChangeHalfDay(halfDayCheckbox) {
+    // This function is called when the half day checkbox is changed
+    // It updates the half day field in the form
+    const custom_period_div = document.getElementById('custom_period_div');
+    const date_to_div = document.getElementById('date_to_div');
+
+    const customHoursInput=document.getElementById("custom_hours")
+
+
+    console.log("Half day checkbox changed:", halfDayCheckbox.checked);
+    if (halfDayCheckbox.checked) {
+        customHoursInput.checked=false;
+        custom_period_div.classList.remove('d-none');
+        custom_period_div.classList.add('d-block');
+        date_to_div.classList.add('d-none');
+        date_to_div.classList.remove('d-block');
+
+
+    } else {
+        date_to_div.classList.remove('d-none');
+        date_to_div.classList.add('d-block');
+
+        custom_period_div.classList.add('d-none');
+        custom_period_div.classList.remove('d-block');
+    }
+}
+
+
+function onChangeCustomHour(customHourCheckBox){
+    const halfDayInput=document.getElementById('half_day')
+    const custom_period_div = document.getElementById('custom_period_div');
+    const date_to_div = document.getElementById('date_to_div');
+
+
+
+    if (customHourCheckBox.checked)
+    {
+        halfDayInput.checked=false;
+        date_to_div.classList.add('d-none');
+        date_to_div.classList.remove('d-block');
+        custom_period_div.classList.add('d-none');
+        custom_period_div.classList.remove('d-block')
+    }
+    else{
+        date_to_div.classList.add('d-block');
+        date_to_div.classList.remove('d-none');
+    }
+}
