23c973948fd7761c8f5a8afe5a38744634501b23
CatherineAmir <catherine.amir20@gmail.com>
2025-08-17 15:12:19 +0300

    add attendance

--- controller/attendance_controller.py
+++ controller/attendance_controller.py
@@ -14,6 +14,7 @@ from odoo.exceptions import AccessError, MissingError
 
 from dateutil.relativedelta import relativedelta
 
+
 class ModelName(CustomerPortal):
 
     def _prepare_home_portal_values(self, counters):
@@ -23,128 +24,134 @@ class ModelName(CustomerPortal):
         employee_id = request.env['hr.employee'].sudo().search([('user_id', '=', user_id.id)])
 
         if 'attendance_count' in counters:
-            attendance_count=request.env['hr.attendance'].sudo().search([('employee_id', '=', employee_id.id),("check_in","!=","technical")])
+            attendance_count = request.env['hr.attendance'].sudo().search(
+                [('employee_id', '=', employee_id.id), ("in_mode", "!=", "technical")])
             values['attendance_count'] = attendance_count
         return values
 
     @http.route(['/my/attendance', '/my/attendance/page/<int:page>'], type='http', auth="user", website=True)
     def portal_my_attendance(self, page=1, sortby=None, filterby=None, **kw):
-        values = self._prepare_my_attendance_values(page, sortby, filterby)
-        request.session['attendance_history'] = values['attendance'][:100]
-        # return request.render("employee_portal.portal_my_time_offs", values)
+        values = self._prepare_my_attendance_values(page=page, sortby=sortby, filterby=filterby)
+        # request.session['attendance_history'] = values['attendance'][:100]
+        return request.render("employee_portal.portal_my_attendance", values)
 
-    def _prepare_my_attendance_values(self, sortby, filterby, domain=None, page=1,
-                                     url="/my/attendance"):
+    def _prepare_my_attendance_values(self, page, sortby, filterby, url="/my/attendance"):
 
         values = {}
 
-        attendance=request.env['hr.attendance']
+        Attendance = request.env['hr.attendance']
         user_id = request.env.user
 
         employee_id = request.env['hr.employee'].sudo().search([('user_id', '=', user_id.id)])
-        domain = expression.AND([domain or [], [('employee_id', '=', employee_id.id),("in_mode","!=","technical")]])
+        domain = expression.AND([[], [('employee_id', '=', employee_id.id)]])
         searchbar_sortings = {
-            'date': {'label': _('Date'), 'order': 'check_in desc'},
+            'date': {'label': _('Date Desc'), 'order': 'check_in desc'},
+            # 'date_2': {'label': _('Date Asc'), 'order': 'check_in'},
 
         }
+
         # default sort by order
         if not sortby:
             sortby = 'date'
-        # order = searchbar_sortings[sortby]['order']
+
+        order = searchbar_sortings[sortby]['order']
 
         searchbar_filters = {
             'all': {'label': _('All'), 'domain': []},
-            'to_approve': {'label': _('To Approve'), 'domain': [("state", 'in', ['confirm', ])]},
-            'refuse': {'label': _('Refuse'), 'domain': [("state", '=', 'refuse')]},
-            'second_approval': {'label': _('Second Approval'), 'domain': [("state", '=', 'validate1')]},
-            'approved': {'label': _('Approved'), 'domain': [("state", '=', 'validate')]},
-            'cancellation_request': {'label': _('Cancellation Request'),
-                                     'domain': [("state", '=', 'cancellation_request')]},
-            'cancel': {'label': _('Cancelled'), 'domain': [("state", '=', 'cancel')]},
 
         }
         # default filter by value
         if not filterby:
             filterby = 'all'
         domain += searchbar_filters[filterby]['domain']
-        all_dates=attendance.sudo().search(domain).mapped("check_in")
-        print("all_dates",all_dates)
-        if not all_dates:
-            return {"pager":{},"attendance":[]}
+        all_dates = Attendance.sudo().search(domain).mapped("check_in")
 
-        period_starts=[]
-        seen=set()
+        if not all_dates:
+            return {"pager": {}, "attendance": []}
+
+        period_starts = []
+        seen_dates = set()
         for dt in sorted(all_dates):
-            print("dt",dt)
-            start,z=self._custom_month_range(dt)
-            print("start",z)
-            print("end",dt)
-            if start not in seen:
-                seen.add(start)
-                period_starts.append(start)
-        print("period_starts",period_starts)
-        total_months = len(period_starts)
+
+            start, end = self._custom_month_range(dt)
+            # print("start",start)
+
+            if start not in seen_dates:
+                seen_dates.add((start,end))
+                # period_starts.append((start)
+
+        seen = sorted(seen_dates, key=lambda t: t[0], reverse=True)
+        total_months = len(seen)
+
         page = max(1, min(page, total_months))
-        month_start = period_starts[page - 1]
-        month_end = month_start + relativedelta(months=1)
+        month_start = list(seen)[page - 1][0]
+        # print("month_start",month_start)
+
+
+
+        month_end = list(seen)[page - 1][1]
+
         month_domain = expression.AND([domain, [
             ('check_in', '>=', month_start),
             ('check_in', '<', month_end),
         ]])
-        print("month_start",month_start)
-        print("month_domain",month_domain)
-        print("month_end",month_end)
-        domain_calculated=expression.AND([domain,month_domain,[("not_calculated_hours","=",False)]])
-        print("domain_calculated",domain_calculated)
-        attendance=attendance.sudo().read_group(
-            fields=["in_mode", "worked_hours", "not_calculated_hours"],
-            domain=domain_calculated, limit=self._items_per_page, orderby="check_in desc",
-            groupby=['check_in:day',"in_mode"], lazy=False),
 
-        pager = {
-            'total': total_months,
-            'page': page,
-            'has_prev': page > 1,
-            'has_next': page < total_months,
-            'prev': page - 1 if page > 1 else None,
-            'next': page + 1 if page < total_months else None,
-            'labels': [
-                f"{start.strftime('%d %b %Y')} → {(start + relativedelta(months=1) - timedelta(days=1)).strftime('%d %b %Y')}"
-                for start in period_starts]
-        }
-        print("attendance",attendance)
+        domain_calculated = expression.AND([domain, month_domain])
+
+        total_count = Attendance.sudo().read_group(domain=domain,fields=['__count'],groupby=['check_in:day'])
+
+        pager = portal_pager(
+            page=page,
+            url=url,
+            url_args={'sortby': sortby, 'filterby': filterby},
+            total=len(total_count),
+            step=31,
+
+        )
+
+        attendance = Attendance.sudo().read_group(
+            fields=["in_mode", "worked_hours", "not_calculated_hours"],
+            domain=domain_calculated, orderby="check_in desc",
+            groupby=['check_in:day', "in_mode", "not_calculated_hours", "is_leave"], lazy=False),
+
+        total_hours = round(sum(
+            round(attendance[0][i]["worked_hours"], 2) if not attendance[0][i]['not_calculated_hours'] else 0 for i in
+            range(0, len(attendance[0]))), 2)
 
         values.update({
-            # attendance.sudo().read_group(
-            #     fields=["in_mode", "worked_hours", "not_calculated_hours"],
-            #     domain=domain, limit=self._items_per_page, offset=pager['offset'],
-            #     groupby=['check_in:month', "check_in:day"], lazy=False),
-
-            'attendance':attendance,
+            'attendance': attendance,
             'page_name': 'attendance',
             'pager': pager,
             'default_url': url,
-            'searchbar_sortings': searchbar_sortings,
+
             'sortby': sortby,
             'searchbar_filters': OrderedDict(sorted(searchbar_filters.items())),
+
+            'searchbar_sortings': searchbar_sortings,
+
             'filterby': filterby,
-            'month_start': month_start,
-            'month_end': month_end,
+            'month_start': month_start.date().strftime('%d %B %Y'),
+
+            'month_end': (month_end.date()-timedelta(days=1)).strftime('%d %B %Y'),
+            'total_hours': total_hours,
 
         })
 
-        print("values",values)
+
 
         return values
 
-    def _custom_month_range(self,ref_date):
+    def _custom_month_range(self, ref_date):
         """
         Returns start and end datetime for the custom month
         starting on the 21st of one month and ending on the 20th of the next.
         """
         if ref_date.day >= 21:
-            start = ref_date.replace(day=21)
+            start = ref_date.replace(day=21).date()
+
         else:
-            start = (ref_date - relativedelta(months=1)).replace(day=21)
-        end = start + relativedelta(months=1)
+            start = ((ref_date - relativedelta(months=1)).replace(day=21)).date()
+
+        end = (start + relativedelta(months=1))
+
         return fields.Datetime.to_datetime(start), fields.Datetime.to_datetime(end)
